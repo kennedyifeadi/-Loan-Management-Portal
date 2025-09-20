5bb4b98fba361a28890a76f13a30b0a618cd6f13
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-09-20 18:40:02 +0100

    feat: implement AuthSlice and UiSlice for authentication and UI state management

---
+++ src/store/slices/UiSlice.ts
@@ -0,0 +1,28 @@
+// Manages UI state like modals, theme, etc.
+import { createSlice } from '@reduxjs/toolkit';
+
+interface UIState {
+    isModalOpen: boolean;
+}
+const initialState: UIState = {
+    isModalOpen: false,
+};
+
+const uiSlice = createSlice({
+    name: 'ui',
+    initialState,
+    reducers: {
+        openModal: (state) => {
+            state.isModalOpen = true;
+        },
+        closeModal: (state) => {
+            state.isModalOpen = false;
+        },
+        toggleModal: (state) => {
+            state.isModalOpen = !state.isModalOpen;
+        },
+    },
+});
+
+export const { openModal, closeModal, toggleModal } = uiSlice.actions;
+export default uiSlice.reducer;
---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+// The central store where we plug everything together
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/AuthSlice';
+import uiReducer from './slices/UiSlice';
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        ui: uiReducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
